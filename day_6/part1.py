f1c8104760bff645e4d806eddb4f32130a85e19b
mani-hash <manimehalan400@gmail.com>
2022-12-06 18:08:00 +0530

    code changes

--- day_6/part1.py
+++ day_6/part1.py
@@ -2,17 +2,16 @@ def find_buff(chk_num):
     with open("input.txt", "r") as f1:
         count = chk_num
         charset = list(f1.read(chk_num))
-        if (len(set(charset)) != len(charset)):
-            while True:
+        while True:
+            if (len(set(charset)) != len(charset)):
                 charset.pop(0)
                 charset.append(f1.read(1))
                 count+=1
+            else:
+                print(count)
+                break
+            
                 
-                if (len(set(charset)) != len(charset)):
-                    continue
-                else:
-                    print(count)
-                    break
 
 
 find_buff(4)
