5a48560d44a5cd6c75b9b8b2ab076887789e6d25
mani-hash <manimehalan400@gmail.com>
2022-12-07 23:20:50 +0530

    formatting

--- day_7/part2.py
+++ day_7/part2.py
@@ -7,26 +7,21 @@ used_space = 0
 direc = part1.dir
 
 candidate = []
+
 for x in direc:
     if (len(direc[x]["dir"]) != 0):
         value = part1.fold_sep(x, direc)
     else:
         value = direc[x]["size"]
-    
     candidate.append(value)
 
 candidate.sort(reverse=True)
 used_space = candidate[0]
 max_space = t_disk - space_need
 
-
-free = 0
-
 for i in range(1, len(candidate)):
     if (used_space - candidate[i] <= max_space):
         free = candidate[i]
-
-
 
 print(free)
 
